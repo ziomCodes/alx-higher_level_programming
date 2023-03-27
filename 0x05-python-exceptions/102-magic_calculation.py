6f7adf0ccd0ca10c7d782b5f84c209241045113a
ziomCodes <ziomcode@gmail.com>
2023-03-27 23:30:37 +0000

    updated code

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -10,3 +11,4 @@ def magic_calculation(a, b):
             result = b + a
             break
     return result
+
