1fec004cee5bd9769aade4e28d244632570ddd0d
ziomCodes <ziomcode@gmail.com>
2023-03-27 22:01:29 +0000

    updated code

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        result = a/b
-    except:
-        result = None
+        quotient = a / b
+    except ZeroDivisionError:
+        quotient = None
     finally:
-        print("Inside result: {}".format(result))
-    return (result)
-
+        print("Inside result: {}".format(quotient))
+        return quotient
