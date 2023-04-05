466f36796f0c16478e015e31f78d17d67586785a
ziomCodes <ziomcode@gmail.com>
2023-04-05 05:36:51 +0000

    Updated code

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,29 +1,40 @@
-i#!/usr/bin/python3
-
-"""Defines a text-indentation function."""
+#!/usr/bin/python3
+'''Contains a text_indentation function for a TDD project.
+'''
 
 
 def text_indentation(text):
-    """Print text with two new lines after each '.', '?', and ':'.
+    '''Prints a text with 2 new lines after each of
+    these characters: '.', '?' and ':'
     Args:
-        text (string): The text to print.
+        text (str): The text to print.
     Raises:
-        TypeError: If text is not a string.
-    """
+        TypeError: If the text is not a string.
+    '''
     if not isinstance(text, str):
-        raise TypeError("text must be a string")
-
-    c = 0
-    while c < len(text) and text[c] == ' ':
-        c += 1
-
-    while c < len(text):
-        print(text[c], end="")
-        if text[c] == "\n" or text[c] in ".?:":
-            if text[c] in ".?:":
-                print("\n")
-            c += 1
-            while c < len(text) and text[c] == ' ':
-                c += 1
-            continue
-        c += 1
+        raise TypeError('text must be a string')
+    else:
+        res = []
+        a = 0
+        text_len = len(text)
+        skip_spaces = True
+        is_end = False
+        is_delim = False
+        for i in range(text_len):
+            is_end = i == text_len - 1
+            is_delim = text[i] in '.?:'
+            if is_delim or is_end:
+                res.append(text[a: i + 1] + ('\n\n' * is_delim))
+                skip_spaces = True
+                a = i + 1
+            elif text[i] in ' \t\r\v' and skip_spaces:
+                a += 1
+            elif text[i] == '\n':
+                res.append(text[a: i + 1].rstrip() + '\n')
+                a += 1
+                skip_spaces = True
+            else:
+                if skip_spaces:
+                    a = i
+                skip_spaces = False
+        print(''.join(res), end='')
