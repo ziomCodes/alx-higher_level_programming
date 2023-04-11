3cd0b5f192c7f4c470f8e8e8e2d10cbe61831eb9
ziomCodes <ziomcode@gmail.com>
2023-04-11 19:52:52 +0000

    input updates

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,39 +1,78 @@
 #!/usr/bin/python3
-"""This documents gather stats from stdin"""
-import sys
+'''A script for parsing HTTP logs.
+'''
+import re
 
 
-def print_pretty(size, code_dict):
-    """parse important data"""
-    print("File size: {}".format(size))
-    for key, value in sorted(code_dict.items()):
-        if (value != 0):
-            print("{}: {}".format(key, value))
+status_codes_stats = {
+    '200': 0,
+    '301': 0,
+    '400': 0,
+    '401': 0,
+    '403': 0,
+    '404': 0,
+    '405': 0,
+    '500': 0
+}
+'''Stats for the supported status codes.
+'''
+total_file_size = 0
+'''The cummulative sum of the file sizes in each HTTP log.
+'''
+fp = (
+    r'\s*(?P<ip>\S+)\s*',
+    r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
+    r'\s*"(?P<request>[^"]*)"\s*',
+    r'\s*(?P<status_code>\S+)',
+    r'\s*(?P<file_size>\d+)'
+)
+'''The pattern for each field in the log.
+'''
+log_fmt = f'{fp[0]}\\-{fp[1]}{fp[2]}{fp[3]}{fp[4]}\\s*'
+'''The regex pattern for the log.
+'''
+
+
+def print_statistics():
+    '''Prints the accumulated statistics of the HTTP log.
+    '''
+    global total_file_size, status_codes_stats
+    print('File size: {:d}'.format(total_file_size), flush=True)
+    for status_code in sorted(status_codes_stats.keys()):
+        num = status_codes_stats.get(status_code, 0)
+        if num > 0:
+            print('{:s}: {:d}'.format(status_code, num), flush=True)
+
+
+def get_metrics(line):
+    '''Retrieves the metrics from a given HTTP log.
+    Args:
+        line (str): The line of input from which to retrieve the metrics.
+    '''
+    global total_file_size, log_fmt, status_codes_stats
+    resp_match = re.fullmatch(log_fmt, line)
+    if resp_match is not None:
+        status_code = resp_match.group('status_code')
+        file_size = int(resp_match.group('file_size'))
+        total_file_size += file_size
+        if status_code in status_codes_stats.keys():
+            status_codes_stats[status_code] += 1
+
+
+def run():
+    '''Starts the log parser.
+    '''
+    line_num = 0
+    try:
+        while True:
+            line = input()
+            get_metrics(line)
+            line_num += 1
+            if line_num % 10 == 0:
+                print_statistics()
+    except (KeyboardInterrupt, EOFError):
+        print_statistics()
+
 
 if __name__ == '__main__':
-    """init code to print the parsed data"""
-    size = 0
-    code_dict = {
-        "200": 0,
-        "301": 0,
-        "400": 0,
-        "401": 0,
-        "403": 0,
-        "404": 0,
-        "405": 0,
-        "500": 0
-    }
-    try:
-        line_counter = 0
-        for line in sys.stdin:
-            line_counter += 1
-            code = line.split()[7]
-            size += int(line.split()[8])
-            if code in code_dict:
-                code_dict[code] += 1
-            if (line_counter % 10 == 0):
-                print_pretty(size, code_dict)
-        print_pretty(size, code_dict)
-    except KeyboardInterrupt:
-        print_pretty(size, code_dict)
-        raise
+    run()
