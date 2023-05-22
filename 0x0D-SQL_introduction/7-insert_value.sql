29a82837523d7ce55a18ddb2593b0b3e67c34119
ziomCodes <ziomcode@gmail.com>
2023-05-22 18:58:52 +0000

    updated sql

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,2 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Inserts a record in a table in the database
+INSERT INTO first_table VALUES (89, 'Best School');
