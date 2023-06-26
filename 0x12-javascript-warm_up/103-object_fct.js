727cd50f7b4835a9fa5b2c550200dc8fa6a63c2a
ziomCodes <ziomcode@gmail.com>
2023-06-26 19:43:54 +0000

    updated warmup

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
-let myObject = {
+const myObject = {
   type: 'object',
   value: 12
 };
 console.log(myObject);
-
-myObject.incr = function () {
-  this.value++;
-};
-
+myObject.incr = function () { myObject.value += 1; };
 myObject.incr();
 console.log(myObject);
 myObject.incr();
