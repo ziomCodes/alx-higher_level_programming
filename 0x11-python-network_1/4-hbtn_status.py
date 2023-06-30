f57a5ee3afe1bed56a14bf65007d5e90c7625e3b
ziomCodes <ziomcode@gmail.com>
2023-06-30 12:40:02 +0000

    updated network

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,15 +1,10 @@
 #!/usr/bin/python3
-"""initializate"""
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
 
-def status():
-    """a Python script that fetches https://intranet.hbtn.io/status"""
-
-    data_req = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}".
-          format(type(data_req.text), data_req.text))
-
-
-if __name__ == '__main__':
-    status()
+if __name__ == "__main__":
+    r = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
